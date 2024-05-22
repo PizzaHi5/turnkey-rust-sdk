0f7f260bfbc5687bb06f97451f30ed2e62910699
PizzaHi5 <pizzahi5@gmail.com>
2024-05-22 10:02:50 -0700

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -19,21 +19,3 @@ pub async fn get_wallet_v1(turnkey:&Turnkey, wallet_id: String) -> TurnkeyResult
     //   }
     Ok(())
 }
-
-// ref: https://docs.turnkey.com/api#tag/Wallets/operation/GetWalletAccounts
-
-//ref: https://docs.turnkey.com/api#tag/Wallets/operation/GetWallets
-
-//ref: https://docs.turnkey.com/api#tag/Wallets/operation/CreateWallet
-
-
-
-//ref: https://docs.turnkey.com/api#tag/Wallets/operation/CreateWalletAccounts
-
-//ref: https://docs.turnkey.com/api#tag/Wallets/operation/ExportWallet
-
-//ref: https://docs.turnkey.com/api#tag/Wallets/operation/ExportWalletAccount
-
-//ref: https://docs.turnkey.com/api#tag/Wallets/operation/ImportWallet
-
-//ref: https://docs.turnkey.com/api#tag/Wallets/operation/InitImportWallet
